53a9aef04aa9e71cb27ef0407a9987f0a1802757
HashedDan <dmangum@hashedhealth.com>
2017-10-09 23:16:03 -0500

    update

--- main.cpp
+++ main.cpp
@@ -5,9 +5,9 @@ using namespace std;
 
 int main (int argc, char * argv[])
 {
-	for (int i = 0; i < argc; ++i) {
-		cout << argv[i] << endl;
-	}
+	// for (int i = 0; i < argc; ++i) {
+	// 	cout << argv[i] << endl;
+	// }
 
 	cout << stoi(argv[1]) + stoi(argv[2]) << endl;
 
